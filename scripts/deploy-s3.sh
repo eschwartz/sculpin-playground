d2228bc0390c378da28524cc4a69d9c133fc4282
Edan Schwartz <edan@edanschwartz.com>
2015-02-26 08:37:55 -0600

    Deploy script accepts args and env vars

--- scripts/deploy-s3.sh
+++ scripts/deploy-s3.sh
@@ -1,11 +1,47 @@
 #!/bin/sh
 # From https://github.com/rudimeier/bash_ini_parser
-. scripts/read_ini.sh
-read_ini s3.ini
+
+if [[ "$1" != "" ]]; then
+  DEPLOY_ENV=$1
+elif [[ "$TRAVIS_BRANCH" != "" ]]; then
+  DEPLOY_ENV=$TRAVIS_BRANCH
+else
+  echo "Unable to determine deploy environment"
+  exit 1
+fi
+
+echo "Generating Sculpin content for $DEPLOY_ENV environment."
 
 # Generate content
-rm -r output_prod
-./sculpin generate --env=prod
+rm -r output_$DEPLOY_ENV
+./sculpin generate --env=$DEPLOY_ENV
+
+echo "\nContent generation successful."
+
 
 # Deploy to s3
-aws s3 sync ./output_prod/ $INI__s3path --profile $INI__profile
+# Ini parser from http://www.tuxz.net/blog/Parse_ini_files_with_bash_and_sed/
+CONFIG_FILE="s3.ini"
+SECTION="$DEPLOY_ENV"
+
+eval `sed -e 's/[[:space:]]*\=[[:space:]]*/=/g' \
+    -e 's/;.*$//' \
+    -e 's/[[:space:]]*$//' \
+    -e 's/^[[:space:]]*//' \
+    -e "s/^\(.*\)=\([^\"']*\)$/\1=\"\2\"/" \
+   < $CONFIG_FILE \
+    | sed -n -e "/^\[$SECTION\]/,/^\s*\[/{/^[^;].*\=.*/p;}"`
+
+echo "Deploying to $s3Path";
+
+if [[ $TRAVIS == true ]]; then
+  aws s3 sync ./output_$DEPLOY_ENV $s3Path
+else
+  echo "\nusing $awsProfile AWS profile";
+  echo "aws s3 sync ./output_$DEPLOY_ENV $s3Path --profile $awsProfile"
+fi
+
+echo "\n"
+echo "Deployment successful."
+
+exit 0;
